5db8630841f6ab754bd3cebc862ca1d715122296
Lee Seung Hoon <aanna0701@gmali.com>
2022-01-21 01:11:46 +0900

    update

--- models/coatnet.py
+++ models/coatnet.py
@@ -9,7 +9,7 @@ from einops.layers.torch import Rearrange
 
 
 def conv_3x3_bn(inp, oup, image_size, downsample=False):
-    stride = 1 if downsample == False else 2
+    stride = 1
     return nn.Sequential(
         nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
         nn.BatchNorm2d(oup),
