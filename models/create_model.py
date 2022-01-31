9ccf924880a522cc4035622aa3ca3f93af75420b
Lee Seung Hoon <aanna0701@gmali.com>
2022-01-31 16:46:51 +0900

    update

--- models/create_model.py
+++ models/create_model.py
@@ -20,7 +20,7 @@ def create_model(img_size, n_classes, args):
                         stochastic_depth=0.1, is_SPT=args.is_SPT, is_LSA=args.is_LSA, is_Coord=args.is_Coord)
         else:
             model = ViT(img_size=224, patch_size = 16,
-                          num_classes=1000, num_classes_trans=n_classes,dim=192, 
+                          num_classes=1000, dim=192, 
                     mlp_dim_ratio=4, depth=12, heads=3, dim_head=192//3,
                     stochastic_depth=0.1, is_SPT=args.is_SPT, is_LSA=args.is_LSA, is_Coord=args.is_Coord)
 
