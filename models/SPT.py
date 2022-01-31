1fb6037d7e175db7168a565cd14949320b180b3e
Lee Seung Hoon <aanna0701@gmali.com>
2022-01-31 17:15:01 +0900

    d

--- models/SPT.py
+++ models/SPT.py
@@ -13,9 +13,8 @@ class ShiftedPatchTokenization(nn.Module):
         self.exist_class_t = exist_class_t
         self.is_Coord = is_Coord
         self.num_patches = num_patches
-        print(num_patches)
         self.num_patches = self.num_patches // (merging_size**2)
-        
+        print(self.num_patches)
         self.patch_shifting = PatchShifting(merging_size)
         
         patch_dim = (in_dim*5) * (merging_size**2) 
