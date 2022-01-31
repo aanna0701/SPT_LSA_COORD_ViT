fed6e00678b1d42f5421c442cbe75a80a313a211
Lee Seung Hoon <aanna0701@gmali.com>
2022-01-31 20:41:19 +0900

    update

--- measure_cost.py
+++ measure_cost.py
@@ -600,11 +600,7 @@ def main(args):
         
 
     elif args.type == 'flops':
-        if args.model in ['vit', 'swin_t','swin_s','swin_b','swin_l', 'pit', 
-          'cait_xxs24', 'cait_xs24', 'cait_s24', 'cait_xxs36', 't2t', 'effiv2', 
-          'regnetX_400m', 'regnetY_4G', 'regnetY_8G', 'effiv2_m', 'regnetX_200m', 'regnetY_400m', 'regnetY_200m',
-          'coatnet_0', 'coatnet_1', 'coatnet_2', 'coatnet_3', 'coatnet2_0', 'coatnet2_1', 
-          'coatnet3_0']:
+        if args.model in ['effiv2', 'regnetX_400m', 'effiv2_m', 'regnetY_200m', 'regnetY_4G', 'regnetY_8G', 'regnetY_400m', 'regnetX_200m', 'res110', 'effib0']:
             # CNN flops
             
             with torch.cuda.device(GPU):
