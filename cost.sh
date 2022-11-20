610adfc2c629cef155622b03f72e167f2695e4b9
Lee SeungHoon <56573275+aanna0701@users.noreply.github.com>
2022-11-20 14:39:14 +0900

    Delete cost.sh

--- cost.sh
+++
@@ -1,6 +0,0 @@
-for m in coatnet_0
-do
-	python measure_cost.py --model ${m} --is_Coord --is_SPT --is_LSA --type flops --dataset T-IMNET
-	python measure_cost.py --model ${m} --type flops --dataset T-IMNET
-
-done
